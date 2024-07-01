d1ea675ef36b4d8a93a33a03dfc6ef3f777a8073
Lev Enders <leveders@mail.ru>
2024-07-01 20:49:45 +0300

    feat: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vitest/config"
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  base: "./",
+  base: './',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      components: '/src/components',
+      types: '/src/types',
+      store: '/src/store',
+    },
+  },
   server: {
     open: true,
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests',
     mockReset: true,
   },
 })
