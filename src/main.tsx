2be91fa3a0776c2d64882e4b85850531b1d05dd9
Lev Enders <leveders@mail.ru>
2024-07-02 20:55:56 +0300

    refactor: set import style

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+
 import { Provider } from 'react-redux'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 import { store } from './store'
