ed73ba1377b7aa647e8be4c50c8abed31cffa6ba
Lev Enders <leveders@mail.ru>
2024-07-05 21:05:52 +0300

    fix: update auth hook

--- src/hooks/firebase.ts
+++ src/hooks/firebase.ts
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react'
+import { useAppSelector } from '@/hooks'
+
 export function useAuth() {
-  const user = localStorage.getItem('user')
+  const [isAuth, setIsAuth] = useState(!!localStorage.getItem('user'))
+  const { id } = useAppSelector(state => state.user)
+
+  useEffect(() => {
+    if (id) {
+      setIsAuth(true)
+    } else {
+      setIsAuth(false)
+    }
+  }, [id, isAuth])
 
   return {
-    isAuth: !!user,
+    isAuth,
   }
 }
