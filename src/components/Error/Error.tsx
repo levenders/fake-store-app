ba6eb2aee429a54c1a540cb8545aea0826148677
Lev Enders <leveders@mail.ru>
2024-07-09 21:27:17 +0300

    fix: removed memo from Error component

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,14 +1,12 @@
-import { memo } from 'react'
-
 import { Headling } from '@/components'
 
 import s from './Error.module.css'
 
-export const Error = memo(function Loader() {
+export const Error = () => {
   return (
     <div className={s.container}>
       <Headling>Кажется что то пошло не так...</Headling>
       <span className={s.error} />
     </div>
   )
-})
+}
